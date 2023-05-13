b2acadf40ec2bf17acdbec65583233657541ebc5
Yesenia <yeseniaaag9@gmail.com>
2023-05-13 17:09:04 -0500

    updated dashboard styling

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -40,22 +40,22 @@ function DataTable() {
         open={open}
         onClose={handleClose}
       />
-      <div className="flex flex-row">
+      <div className="flex flex-row items-center justify-center p-10">
         <div>
           <button
-            className="p-3 bg-slate-300 m-3 rounded hover:bg-slate-800 hover:text-white"
+            className="p-3 bg-slate-300 m-3 pl-4 pr-4 rounded hover:bg-slate-800 hover:text-white"
             onClick={() => handleOpen()}
           >
             Create New Whisky
           </button>
         </div>
-        <Button onClick={handleOpen} className="p-3 bg-slate-300 m-3 rounded hover:bg-slate-800 hover:text-white">Update</Button> 
-        <Button onClick={deleteData} className="p-3 bg-slate-300 m-3 rounded hover:bg-slate-800 hover:text-white">Delete</Button>
+        <Button onClick={handleOpen} className="p-3 bg-slate-300 m-3 pl-10 pr-10 rounded hover:bg-slate-800 hover:text-white">Update</Button> 
+        <Button onClick={deleteData} className="p-3 bg-slate-300 m-3 pl-10 pr-10 rounded hover:bg-slate-800 hover:text-white">Delete</Button>
       </div>
       <div className={ open ? "hidden" : "container mx-10 my-5 flex flex-col"} 
         style={{ height: 400, width: '100%' }}
         >
-          <h2 className="p-3 bg-slate-300 my-2 rounded">My Whiskys</h2>
+          <h2 className="p-3 bg-slate-600 text-white pl-4 my-2 rounded">My Whiskys</h2>
           <DataGrid rows={whiskyData} columns={columns} rowsPerPageOptions={[5]} 
           checkboxSelection={true}
           onSelectionModelChange={ (item:any) => {
